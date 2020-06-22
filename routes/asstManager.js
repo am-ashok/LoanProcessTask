b6601465cd818c152b0262cabcc0bebf70605526
AK <amashokkumar@hotmail.com>
2020-06-22 23:38:07 +0530

    JSON routes assigned to customer bank and asst manager

--- routes/asstManager.js
+++ routes/asstManager.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('Bank Manager');
+  res.json('Bank Manager');
 });
 
 module.exports = router;
